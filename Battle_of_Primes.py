3cfd32ede7c7af5b00862ddee98f244c1c9ad082
SriVidya2003 <98937360+SriVidya2003@users.noreply.github.com>
2022-06-29 17:29:53 +0100

    def prime(n):     for i in range(2,n):         if n%i==0:             return False     else:         return True a=int(input()) b=int(input()) for i in range(1,100):     c=a+b+i     if(prime(c)):         print(i)         break

---
+++ Battle_of_Primes.py
@@ -0,0 +1,14 @@
+def prime(n):
+    for i in range(2,n):
+        if n%i==0:
+            return False
+    else:
+        return True
+a=int(input())
+b=int(input())
+for i in range(1,100):
+    c=a+b+i
+    if(prime(c)):
+        print(i)
+        break
+    
